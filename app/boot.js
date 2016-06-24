e046ad6d3f1ad57956e90472548c8127a523ba83
Grace Hui <grace.hui@carmanah.com>
2016-06-24 13:00:34 -0700

    first commit

---
+++ app/boot.js
@@ -0,0 +1,22 @@
+System.register(['@angular/platform-browser-dynamic', "./app.component", "@angular/http"], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var platform_browser_dynamic_1, app_component_1, http_1;
+    return {
+        setters:[
+            function (platform_browser_dynamic_1_1) {
+                platform_browser_dynamic_1 = platform_browser_dynamic_1_1;
+            },
+            function (app_component_1_1) {
+                app_component_1 = app_component_1_1;
+            },
+            function (http_1_1) {
+                http_1 = http_1_1;
+            }],
+        execute: function() {
+            platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [http_1.HTTP_PROVIDERS]);
+        }
+    }
+});
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImJvb3QudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Ozs7OztZQUtBLG9DQUFTLENBQUMsNEJBQVksRUFBRSxDQUFDLHFCQUFjLENBQUMsQ0FBQyxDQUFDIiwiZmlsZSI6ImJvb3QuanMiLCJzb3VyY2VzQ29udGVudCI6WyIvLy8gPHJlZmVyZW5jZSBwYXRoPVwiLi4vdHlwaW5ncy9icm93c2VyLmQudHNcIiAvPlxyXG5pbXBvcnQge2Jvb3RzdHJhcH0gICAgZnJvbSAnQGFuZ3VsYXIvcGxhdGZvcm0tYnJvd3Nlci1keW5hbWljJztcclxuaW1wb3J0IHtBcHBDb21wb25lbnR9IGZyb20gXCIuL2FwcC5jb21wb25lbnRcIjtcclxuaW1wb3J0IHtIVFRQX1BST1ZJREVSU30gZnJvbSBcIkBhbmd1bGFyL2h0dHBcIjtcclxuXHJcbmJvb3RzdHJhcChBcHBDb21wb25lbnQsIFtIVFRQX1BST1ZJREVSU10pO1xyXG4iXSwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
---
+++ dev/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <header>
+            <h1>Angular 2 </h1>
+        </header>
+    `,
+})
+export class AppComponent {
+}
